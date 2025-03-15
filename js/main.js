2d17dc3714af4c051b7dfc504a95cd4bc6278f59
WilliamSanchez <sanchezwill596@gmail.com>
2025-03-15 12:51:06 -0500

    PRIMER COMMIT

---
+++ js/main.js
@@ -0,0 +1,157 @@
+function circulo(){
+    let elemento = document.getElementById("figura");
+    let titulo = document.getElementById("titulo");
+    elemento.classList.toggle("circulo")
+    if (titulo.textContent == "Figura") {
+        titulo.textContent = "Figura : Circulo"
+    }
+        else {
+            titulo.textContent = "Figura"
+        }
+}
+
+function rectangulo(){
+    let elemento = document.getElementById("figura");
+    let titulo = document.getElementById("titulo");
+    elemento.classList.toggle("rectangulo")
+    if (titulo.textContent == "Figura") {
+        titulo.textContent = "Figura : Rectangulo"
+    }
+        else {
+            titulo.textContent = "Figura"
+        }
+}
+
+function triangulo(){
+    let elemento = document.getElementById("figura");
+    let titulo = document.getElementById("titulo");
+    elemento.classList.toggle("triangulo")
+    if (titulo.textContent == "Figura") {
+        titulo.textContent = "Figura : Triangulo"
+    }
+        else {
+            titulo.textContent = "Figura"
+        }
+}
+
+function rombo () {
+    let elemento = document.getElementById("figura");
+    let titulo = document.getElementById("titulo");
+    elemento.classList.toggle("rombo")
+    if (titulo.textContent == "Figura") {
+        titulo.textContent = "Figura : Rombo"
+    }
+        else {
+            titulo.textContent = "Figura"
+        }
+}
+
+function left (){
+    let elemento = document.getElementById("figura");
+    let titulo = document.getElementById("titulo");
+    elemento.classList.toggle("left");
+    if (titulo.textContent == "Figura") {
+        titulo.textContent = "Figura : Mov Left"
+    }
+        else {
+            titulo.textContent = "Figura"
+        }
+}
+
+function topp(){
+    let elemento = document.getElementById("figura");
+    let titulo = document.getElementById("titulo");
+    elemento.classList.toggle("topp");
+    if (titulo.textContent == "Figura") {
+        titulo.textContent = "Figura : Mov top"
+    }
+        else {
+            titulo.textContent = "Figura"
+        }
+}
+
+function right(){
+    let elemento = document.getElementById("figura");
+    let titulo = document.getElementById("titulo");
+    elemento.classList.toggle("right");
+    if (titulo.textContent == "Figura") {
+        titulo.textContent = "Figura : Mov right"
+    }
+        else {
+            titulo.textContent = "Figura"
+        }
+}
+
+function gift(){
+    let elemento = document.getElementById("figura");
+    let titulo = document.getElementById("titulo");
+    elemento.classList.toggle("gift");
+    if (titulo.textContent == "Figura") {
+        titulo.textContent = "Figura : Gift"
+    }
+        else {
+            titulo.textContent = "Figura"
+        }
+}
+
+function bottom(){
+    let elemento = document.getElementById("figura");
+    let titulo = document.getElementById("titulo");
+    elemento.classList.toggle("bottom");
+    if (titulo.textContent == "Figura") {
+        titulo.textContent = "Figura : Bottom"
+    }
+        else {
+            titulo.textContent = "Figura"
+        }
+}
+
+function diagonal(){
+    let elemento = document.getElementById("figura");
+    let titulo = document.getElementById("titulo");
+    elemento.classList.toggle("diagonal");
+    if (titulo.textContent == "Figura") {
+        titulo.textContent = "Figura : Diagonal"
+    }
+        else {
+            titulo.textContent = "Figura"
+        }
+}
+
+function degradado(){
+    let elemento = document.getElementById("figura");
+    let titulo = document.getElementById("titulo");
+    elemento.classList.toggle("degradado");
+    if (titulo.textContent == "Figura") {
+        titulo.textContent = "Figura : Degradado"
+    }
+        else {
+            titulo.textContent = "Figura"
+        }
+}
+
+function zoomInYZoomOut(){
+    let elemento = document.getElementById("figura");
+    let titulo = document.getElementById("titulo");
+    elemento.classList.toggle("zoomInYZoomOut");
+    if (titulo.textContent == "Figura") {
+        titulo.textContent = "Figura : Zoom In / Zoom Out"
+    }
+        else {
+            titulo.textContent = "Figura"
+        }
+}
+
+function zoomInYZoomt() {
+    let elemento = document.getElementById("figura");
+    let titulo = document.getElementById("titulo");
+    let scale = 1;
+    scale += 0.1; // Aumenta el zoom
+    elemento.style.transform = `scale(${scale})`;
+    titulo.textContent = "Figura : Zoom In / Zoom Out";
+    if (scale > 0.5) { // Limite mínimo
+        scale -= 0.1; // Reduce el zoom
+        elemento.style.transform = `scale(${scale})`;
+    }
+    titulo.textContent = "Figura : Zoom In / Zoom Out";
+}
